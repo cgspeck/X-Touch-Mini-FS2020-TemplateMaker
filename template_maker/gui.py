f45dd197a69ffbfd833607567cd0eb251f4ce191
Chris Speck <cgspeck@gmail.com>
2022-12-28 12:23:01 +1100

    Fix image resizing in preview

--- template_maker/gui.py
+++ template_maker/gui.py
@@ -6,14 +6,14 @@ from tkinter import filedialog as fd
 from tkinter.messagebox import showerror
 from typing import Optional, Union
 
-from PIL import Image
+from PIL import Image, ImageTk
 
 
 def make_preview_app(image_file_path: Path) -> tk.Tk:
     screen_width, screen_height = GetSystemMetrics(0), GetSystemMetrics(1)
     img = Image.open(image_file_path)
     image_original_width, image_original_height = img.width, img.height
-    window_width, window_height = min(screen_width // 2, image_original_width), min(screen_height // 2, image_original_height)
+    window_width, window_height = max(screen_width // 2, image_original_width), max(screen_height // 2, image_original_height)
 
     class App(tk.Tk):
         def __init__(self):
@@ -21,9 +21,8 @@ def make_preview_app(image_file_path: Path) -> tk.Tk:
             self.title("Template preview")
             self.geometry("{}x{}".format(window_width, window_height))
             img = Image.open(image_file_path)
-            max_height = img.height
-            img.thumbnail([window_width, max_height], Image.Resampling.LANCZOS)
-            self.python_image = tk.PhotoImage(file=image_file_path)
+            img.thumbnail([window_width, window_height], Image.Resampling.LANCZOS)
+            self.python_image = ImageTk.PhotoImage(img)
             tk.Label(self, image=self.python_image).pack(fill="both", expand=True)
 
     return App
