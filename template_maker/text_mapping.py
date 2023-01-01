0bfe66820e2ae61d1eaa8b45401c84fef6ecdf5c
Chris Speck <cgspeck@gmail.com>
2023-01-01 16:15:33 +1100

    Store original replacement strings, and copy default mappings to user if no user ones exist

--- template_maker/text_mapping.py
+++ template_maker/text_mapping.py
@@ -14,6 +14,7 @@ from template_maker.vars import default_mappings, user_mappings
 class TextMapping(DataClassJsonMixin):
     pat: Pattern
     replacement: str
+    replacement_unsanitized: str
 
 
 def sanitise_replacement(original: str) -> str:
@@ -35,7 +36,13 @@ def _from_disk(fp: Path) -> List[TextMapping]:
         k, v = l.split("=")
         k = k.strip()
         v = v.strip()
-        memo.append(TextMapping(pat=re.compile(k), replacement=sanitise_replacement(v)))
+        memo.append(
+            TextMapping(
+                pat=re.compile(k),
+                replacement=sanitise_replacement(v),
+                replacement_unsanitized=v,
+            )
+        )
     return memo
 
 
@@ -43,7 +50,10 @@ def load_mappings(remove_unrecognized: Optional[bool] = False) -> List[TextMappi
     memo = []
 
     memo.extend(_from_disk(user_mappings))
-    memo.extend(_from_disk(default_mappings))
+
+    if len(memo) == 0:
+        user_mappings.write_text(default_mappings.read_text())
+        return load_mappings(remove_unrecognized)
 
     if remove_unrecognized:
         memo.append(TextMapping(pat=re.compile(r".*"), replacement=""))
