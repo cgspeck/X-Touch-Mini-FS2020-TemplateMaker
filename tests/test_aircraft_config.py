84be72fda06132b3ae6578a13e9f387f6b36d5d0
Chris Speck <cgspeck@gmail.com>
2023-01-05 20:09:48 +1100

    Add test that config reader deduplicates strings

--- tests/test_aircraft_config.py
+++ tests/test_aircraft_config.py
@@ -11,7 +11,40 @@ def config_generic_ap() -> str:
     return Path("tests", "data", "config_generic_ap.json")
 
 
+@pytest.fixture()
+def config_a320() -> str:
+    return Path("tests", "data", "config_a320_modded.json")
+
+
 def test_parse_generic_ap(config_generic_ap: str, snapshot: SnapshotFixture) -> None:
     actual = parse_aircraft_config(config_generic_ap).to_dict()
-    print(actual["encoders"][7])
     assert snapshot("json") == actual
+
+
+def test_dedupes_strings(config_a320: str, snapshot: SnapshotFixture) -> None:
+    actual = parse_aircraft_config(config_a320)
+
+    encoder1 = actual.encoders[0]
+    assert encoder1.layer_a_primary_text.original == "KOHLSMAN_INC"
+    assert encoder1.layer_a_secondary_text.original is None
+    assert encoder1.layer_a_tertiary_text.original is None
+
+    encoder2 = actual.encoders[1]
+    assert encoder2.layer_a_primary_text.original == "AP_SPD_VAR_INC"
+    assert encoder2.layer_a_secondary_text.original == "SPEED_SLOT_INDEX_SET"
+    assert encoder2.layer_a_tertiary_text.original is None
+
+    encoder3 = actual.encoders[2]
+    assert encoder3.layer_a_primary_text.original == "HEADING_BUG_INC"
+    assert encoder3.layer_a_secondary_text.original == "HEADING_SLOT_INDEX_SET"
+    assert encoder3.layer_a_tertiary_text.original is None
+
+    encoder4 = actual.encoders[3]
+    assert encoder4.layer_a_primary_text.original == "AP_ALT_VAR_INC"
+    assert encoder4.layer_a_secondary_text.original is None
+    assert encoder4.layer_a_tertiary_text.original == "ALTITUDE_SLOT_INDEX_SET"
+
+    encoder5 = actual.encoders[4]
+    assert encoder5.layer_a_primary_text.original == "FOO"
+    assert encoder5.layer_a_secondary_text.original == "BAR"
+    assert encoder5.layer_a_tertiary_text.original is None
